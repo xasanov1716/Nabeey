3c36d51796c54e900ebca065d865c3df4ffca883
Dilshodbek0720 <sayitqulovdilshodbek@gmail.com>
2023-09-28 18:06:55 +0500

    added video_detail_screen

--- lib/ui/tab/home/sub_screens/videos/sub_screens/video_detail/video_detail_screen.dart
+++ lib/ui/tab/home/sub_screens/videos/sub_screens/video_detail/video_detail_screen.dart
@@ -1,6 +1,7 @@
+import 'package:contest_app/ui/tab/home/sub_screens/videos/sub_screens/video_detail/widgets/chewie.dart';
 import 'package:contest_app/utils/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoDetailScreen extends StatefulWidget {
   const VideoDetailScreen({super.key});
@@ -10,25 +11,41 @@ class VideoDetailScreen extends StatefulWidget {
 }
 
 class _VideoDetailScreenState extends State<VideoDetailScreen> {
+  final List<String> videoUrls = [
+    "https://files.nevomusic.net/video/Klip/Jaloliddin%20Ahmadaliyev%20%20Ozim%20Mood%20video.mp4",
+    "https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4",
+    'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4'
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Lorem Ipsum",
-          style: TextStyle(
-              fontFamily: "Poppins",
-              fontSize: 24.sp,
-              fontWeight: FontWeight.w700,
-              color: AppColors.C_111111),
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+          ),
         ),
-      ),
-
-      body: Column(
-        children: [
-
-        ],
-      ),
+        body: ListView(
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              child: Text("Ikrom Sharif videolari", style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24,
+                  color: AppColors.C_111111
+              ),),
+            ),
+            ...List.generate(videoUrls.length, (index) => VideoItem(
+              videoPlayerController: VideoPlayerController.networkUrl(
+                Uri.parse(videoUrls[index]),
+              ),
+            ))
+          ],
+        )
     );
   }
 }
