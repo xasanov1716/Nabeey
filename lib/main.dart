2ed14461386c68d97343f8d333421040634d78ec
Nodirchik <nodirchiksoliyev.@gmail.com>
2023-09-29 07:11:05 +0500

    article_detail_screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:contest_app/blocs/audio_bloc/audio_bloc.dart';
 import 'package:contest_app/ui/tab/app_routes.dart';
-import 'package:contest_app/ui/tab/home/sub_screens/articles/sub_screen/article_detail_screen.dart';
-import 'package:contest_app/ui/tab/home/sub_screens/videos/sub_screens/all_videos/videos_screen.dart';
-import 'package:contest_app/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -42,9 +39,8 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
             useMaterial3: true,
           ),
-          home: const ArticleDetailScreen(),
-          // initialRoute: RouteNames.tabBox,
-          // onGenerateRoute: AppRoutes.generateRoute,
+          initialRoute: RouteNames.tabBox,
+          onGenerateRoute: AppRoutes.generateRoute,
         );
       },
     );
