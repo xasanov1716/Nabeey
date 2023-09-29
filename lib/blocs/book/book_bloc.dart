fe3c55d609c3751b697863eff0d984e6767ee4bc
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-09-29 17:55:17 +0500

    some changes

--- lib/blocs/book/book_bloc.dart
+++ lib/blocs/book/book_bloc.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
-import 'package:contest_app/data/model/book/file_model.dart';
+import 'package:contest_app/data/models/book/book_model.dart';
+import 'package:contest_app/data/models/book/file_model.dart';
 import 'package:contest_app/data/status/form_status.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:contest_app/data/model/book/book_model.dart';
 
 part 'book_event.dart';
 
