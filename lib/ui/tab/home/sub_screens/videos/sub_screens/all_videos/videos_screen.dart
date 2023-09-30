3f6cca44022b2626072a86f67d6364f17128df9c
Nodirchik <nodirchiksoliyev.@gmail.com>
2023-09-30 15:02:29 +0500

    Global AppBar added articles and videos screens fixed

--- lib/ui/tab/home/sub_screens/videos/sub_screens/all_videos/videos_screen.dart
+++ lib/ui/tab/home/sub_screens/videos/sub_screens/all_videos/videos_screen.dart
@@ -1,10 +1,8 @@
 import 'package:contest_app/ui/tab/app_routes.dart';
-import 'package:contest_app/ui/tab/home/sub_screens/videos/sub_screens/all_videos/widgets/main_bar_widget.dart';
 import 'package:contest_app/ui/tab/home/sub_screens/videos/sub_screens/all_videos/widgets/videos_list_widget.dart';
+import 'package:contest_app/ui/widgets/global_app_bar.dart';
 import 'package:contest_app/utils/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../../../../../blocs/video_bloc/video_bloc.dart';
 import '../../../../../../../utils/icons.dart';
 
 class VideosScreen extends StatefulWidget {
@@ -27,53 +25,47 @@ List<String> subtitles = ["Lorem ipsum", "Lorem ipsum", "Lorem ipsum", "Lorem ip
 class _VideosScreenState extends State<VideosScreen> {
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+    List<Widget> videoLists = [
+      ...List.generate(
+        3,
+        (index) => VideosListWidget(
+          images: images,
+          titles: titles,
+          subtitles: subtitles,
+          videosName: "Ustoz#${index + 1} videolari",
+          onTap: () {
+            Navigator.pushNamed(context, RouteNames.videoDetailScreen);
+          },
+        ),
+      ),
+    ];
     return Scaffold(
       backgroundColor: AppColors.white,
-      body: Column(
-        children: [
-          MainBarWidget(
-            image: AppIcons.image2,
-            title: "Lorem Ipsum",
-            subtitle: "Video",
-            buttonText: "Take the quiz",
-            onTab: () {},
+      body: GlobalAppBar(
+        title: "Lorem Ipsum",
+        subtitle: "Video",
+        image: AppIcons.image2,
+        onTap: () {},
+        body: Expanded(
+          child: ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            itemCount: videoLists.length,
+            separatorBuilder: (context, index) {
+              return Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: width / 18),
+                    child: Divider(color: AppColors.black.withOpacity(0.2)),
+                  ),
+                ],
+              );
+            },
+            itemBuilder: (BuildContext context, int index) {
+              return videoLists[index];
+            },
           ),
-          Expanded(
-            child: ListView.separated(
-              physics: const BouncingScrollPhysics(),
-              itemCount: 4,
-              separatorBuilder: (context, index) {
-                return Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: width / 18),
-                      child: Divider(color: AppColors.black.withOpacity(0.2)),
-                    ),
-                    SizedBox(height: height / 35),
-                  ],
-                );
-              },
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  children: [
-                    VideosListWidget(
-                      images: images,
-                      titles: titles,
-                      subtitles: subtitles,
-                      videosName: "Ustoz#${index+1} videolari",
-                      onTap: () {
-                        Navigator.pushNamed(context, RouteNames.videoDetailScreen);
-                      },
-                    ),
-                    SizedBox(height: height / 28),
-                  ],
-                );
-              },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
