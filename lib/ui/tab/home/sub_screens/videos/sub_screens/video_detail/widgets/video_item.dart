315a9895580405f0a044353af1a19ac696aa6c1b
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-10-02 18:26:03 +0500

    some changes

--- lib/ui/tab/home/sub_screens/videos/sub_screens/video_detail/widgets/video_item.dart
+++ lib/ui/tab/home/sub_screens/videos/sub_screens/video_detail/widgets/video_item.dart
@@ -1,12 +1,14 @@
 import 'package:contest_app/data/models/video_model/video_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-
 class VideoItem extends StatefulWidget {
   const VideoItem({super.key, required this.videoModel});
+
   final VideoModel videoModel;
+
   @override
   State<VideoItem> createState() => _VideoItemState();
 }
@@ -14,18 +16,15 @@ class VideoItem extends StatefulWidget {
 class _VideoItemState extends State<VideoItem> {
   late YoutubePlayerController youtubePlayerController;
 
-
-
   @override
   void initState() {
     final videoId = YoutubePlayer.convertUrlToId(widget.videoModel.videoLink);
     youtubePlayerController = YoutubePlayerController(
-      initialVideoId: videoId!,
-      flags: const YoutubePlayerFlags(
-        autoPlay: false,
-        showLiveFullscreenButton: true,
-      )
-    );
+        initialVideoId: videoId!,
+        flags: const YoutubePlayerFlags(
+          autoPlay: false,
+          showLiveFullscreenButton: true,
+        ));
     super.initState();
   }
 
@@ -37,48 +36,74 @@ class _VideoItemState extends State<VideoItem> {
       children: [
         Container(
           height: size.width > 500 ? size.height : 215.h,
-          margin: EdgeInsets.symmetric(horizontal: size.width > 500 ? 0 : 20.w, vertical: size.width > 500 ? 0 : 12.h),
+          margin: EdgeInsets.symmetric(
+              horizontal: size.width > 500 ? 0 : 20.w,
+              vertical: size.width > 500 ? 0 : 12.h),
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(size.width > 500 ? 0 : 20.r),
-            child: YoutubePlayerBuilder(
-              builder: (context, player){
-                return const SizedBox();
-              },
-              player: YoutubePlayer(
-                controller: youtubePlayerController,
-                showVideoProgressIndicator: true,
-                bottomActions: [
-                  CurrentPosition(),
-                  ProgressBar(
-                    isExpanded: true,
-                    colors: ProgressBarColors(
-                      playedColor: const Color(0xFFF59C16),
-                      handleColor: const Color(0xFFF59C16),
-                      backgroundColor: const Color(0xFFFFFFFF).withOpacity(0.5),
+              borderRadius: BorderRadius.circular(size.width > 500 ? 0 : 20.r),
+              child: YoutubePlayerBuilder(
+                onExitFullScreen: () {
+                  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+                },
+                builder: (context, player) {
+                  return const SizedBox();
+                },
+                player: YoutubePlayer(
+                  controller: youtubePlayerController,
+                  showVideoProgressIndicator: true,
+                  bottomActions: [
+                    CurrentPosition(),
+                    ProgressBar(
+                      isExpanded: true,
+                      colors: ProgressBarColors(
+                        playedColor: const Color(0xFFF59C16),
+                        handleColor: const Color(0xFFF59C16),
+                        backgroundColor:
+                            const Color(0xFFFFFFFF).withOpacity(0.5),
+                      ),
                     ),
-                  ),
-                  RemainingDuration(),
-                  FullScreenButton(controller: youtubePlayerController)
-                ],
-              ),
-            )
-          ),
+                    RemainingDuration(),
+                    FullScreenButton(controller: youtubePlayerController)
+                  ],
+                ),
+              )),
         ),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.w),
-          child: const Row(children: [
-            Text("12th, July 2022", style: TextStyle(color: Color(0xFF232323), fontWeight: FontWeight.w400, fontSize: 12),),
-            SizedBox(width: 24,),
-            Text("10.658 views", style: TextStyle(color: Color(0xFF232323), fontWeight: FontWeight.w400, fontSize: 12),),
-          ],),
+          child: const Row(
+            children: [
+              Text(
+                "12th, July 2022",
+                style: TextStyle(
+                    color: Color(0xFF232323),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12),
+              ),
+              SizedBox(
+                width: 24,
+              ),
+              Text(
+                "10.658 views",
+                style: TextStyle(
+                    color: Color(0xFF232323),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12),
+              ),
+            ],
+          ),
         ),
-        SizedBox(height: 8.h,),
-        Padding(padding: EdgeInsets.symmetric(horizontal: 20.w),
-          child: Text(widget.videoModel.title,
+        SizedBox(
+          height: 8.h,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: Text(
+            widget.videoModel.title,
             style: TextStyle(
-            fontSize: 18.sp,
-            fontWeight: FontWeight.w600,
-          ),),
+              fontSize: 18.sp,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
         )
       ],
     );
