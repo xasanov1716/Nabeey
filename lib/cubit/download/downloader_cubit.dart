f8b0f225f6a69116af205b3da7ad717b1301c92b
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-10-06 17:39:13 +0500

    some changes

--- lib/cubit/download/downloader_cubit.dart
+++ lib/cubit/download/downloader_cubit.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:contest_app/cubit/download/downloader_state.dart';
+import 'package:contest_app/ui/tab/profile/auth/auth_screen.dart';
 import 'package:contest_app/utils/loading_dialog.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dio/dio.dart';
@@ -10,32 +11,29 @@ import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-
 class DownLoaderCubit extends Cubit<DownLoaderState> {
   DownLoaderCubit()
       : super(
-    const DownLoaderState(
-      progress: 0.0,
-      newFileLocation: "",
-    ),
-  );
+          const DownLoaderState(
+            progress: 0.0,
+            newFileLocation: "",
+          ),
+        );
 
   myProgressEmitter(double pr) {
     emit(state.copyWith(progress: pr));
   }
 
-  downloadFile({
-    required String fileInfo,
-    required BuildContext context
-  }) async {
+  downloadFile(
+      {required String fileInfo, required BuildContext context}) async {
     bool hasPermission = await _requestWritePermission();
     if (!hasPermission) return;
     Dio dio = Dio();
 
     var directory = await getDownloadPath();
-    String newFileLocation = "${directory?.path}/Nabeey${fileInfo.substring(fileInfo.length - 5, fileInfo.length)}";
+    String newFileLocation =
+        "${directory?.path}/Nabeey${fileInfo.substring(fileInfo.length - 5, fileInfo.length)}";
     var allFiles = directory?.list();
-
 
     List<String> filePaths = [];
     await allFiles?.forEach((element) {
@@ -45,7 +43,7 @@ class DownLoaderCubit extends Cubit<DownLoaderState> {
       OpenFilex.open(newFileLocation);
     } else {
       try {
-        if(context.mounted){
+        if (context.mounted) {
           showLoading(context: context);
         }
         await dio.download(
@@ -58,12 +56,13 @@ class DownLoaderCubit extends Cubit<DownLoaderState> {
             }
           },
         );
-        if(context.mounted){
+        if (context.mounted) {
           hideLoading(context: context);
         }
-  OpenFilex.open(newFileLocation);
+        OpenFilex.open(newFileLocation);
       } catch (error) {
         debugPrint("DOWNLOAD ERROR:$error");
+        if (context.mounted) showErrorDialog(context, error.toString());
       }
     }
   }
--- lib/cubit/download/downloader_state.dart
+++ lib/cubit/download/downloader_state.dart
@@ -1,24 +1,3 @@
-// part of 'downloader_cubit.dart';
-//
-// class DownloaderState  {
-//   const DownloaderState({
-//     required this.progress,
-//     required this.myStream,
-//   });
-//
-//   final double progress;
-//   final StreamController<double> myStream;
-//
-//   DownloaderState copyWith({
-//     double? progress,
-//     StreamController<double>? myStream,
-//   }) =>
-//       DownloaderState(
-//         progress: progress ?? this.progress,
-//         myStream: myStream ?? this.myStream,
-//       );
-// }
-
 import 'package:equatable/equatable.dart';
 
 
